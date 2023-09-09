f15491860725c56921c17c8d92bbf9d63df57558
Konstantin Salyaev <r.cf2003@yandex.ru>
2023-09-09 16:30:13 +0300

    feat: add user

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module authentication
 
 go 1.21
+
+require (
+	github.com/google/uuid v1.3.1 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+)
--- internal/entity/user.go
+++ internal/entity/user.go
@@ -1,1 +1,31 @@
 package entity
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	Id             uint32
+	Email          string
+	HashedPassword string
+}
+
+func NewUser(email, password string) (*User, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot hash password: %w", err)
+	}
+
+	user := &User{
+		Email:          email,
+		HashedPassword: string(hashedPassword),
+	}
+
+	return user, nil
+}
+
+func (u *User) IsPasswordCorrect(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.HashedPassword), []byte(password))
+	return err == nil
+}
